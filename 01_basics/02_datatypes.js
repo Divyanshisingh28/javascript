10afe50a482f5de7dacae7a41aa64454c99ac1e1
divyanshi singh <divyanshisinghs007@gmail.com>
2026-07-26 10:23:45 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,15 @@
 "use strict"; //code will be treated as newer version of javascript 
 
 //alert("hello") //this syntax works on browser and not on nodejs 
+
+let name ="div" //string
+let age = 18 // number range: 2 to the power 53
+let isloggedin = true //boolean
+//null => standalone value (khaali hai)
+//undefined => value nahi di hai koi
+//symbol => for react , uniqueness 
+console.log(typeof(name))
+console.log(typeof null) // object comes (language error hai)
+console.log(typeof undefined)
+
+
