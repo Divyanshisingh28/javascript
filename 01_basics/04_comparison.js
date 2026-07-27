479d63d3df571c17807b0d9c6f72cb4e0d50c549
divyanshi singh <divyanshisinghs007@gmail.com>
2026-07-27 08:31:21 +0000

    comaprisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  // in js == and >= works differently
+
+console.log(null == 0); // isme datatype change nahi hota automatically issliye isme false aata hai 
+console.log(null >= 0); // isme datatype automatically change hojaata hai aur null ko 0 bana deta hai issliye true aata hai
+
+console.log(undefined == 0); // it is not a good practice all together kyuki defined behaviour nahi hai ye 
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
