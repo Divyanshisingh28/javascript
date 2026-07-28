ceed635aa65dafffe90b0b852db0cc07c0e74ecb
divyanshi singh <divyanshisinghs007@gmail.com>
2026-07-28 15:40:20 +0000

    str

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,14 @@
 let name = "divyanshi"
 let bf = "riyan roy"
 console.log(`hello my name is ${name} and my bf is ${bf}`) // this is like f string in python
+let str = new String("divyanshi")
+console.log(typeof str) // string is object here 
+// length property milti hai 
+console.log(str[1]) // 0 based indexing
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str) // orignal value is not changed 
+console.log(str.charAt(7)) // 7th position pe kaunsa character hai ye dekhna hai 
+console.log(str.indexOf("i"))
+
 
