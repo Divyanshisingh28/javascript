36c93de1b5e957a7a484c7e41039cf2886132316
divyanshi singh <divyanshisinghs007@gmail.com>
2026-09-09 10:52:21 +0000

    if else

---
+++ 05_control_flow/one.js
@@ -0,0 +1,19 @@
+// if
+// if(condition == true ){}
+const isuserloggedin = true
+if(isuserloggedin){
+    console.log("welcome to the website")
+}
+ // == :- comparison
+ // === :- checks the datatype as well
+ // = :- assignment operator, it only assigns the value
+
+ if(2==="2"){
+    console.log("its equal")
+ }
+ else console.log("its not equal")
+
+const balance = 1000
+
+// && :- checks both conditions 
+// || :- checks either one is true
