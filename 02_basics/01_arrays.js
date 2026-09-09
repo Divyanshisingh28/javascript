2b17ed671bb00704f280e73908084bf621eac5fe
divyanshi singh <divyanshisinghs007@gmail.com>
2026-09-09 13:46:44 +0530

    new

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,11 @@
-// push , pop , shift , unshift , slice , splice , join , 
+// push , pop , shift , unshift , slice , splice , join 
+const myarr = [1,2,43,5,7]
+myarr.push(30); // adds 30 to the last
+myarr.pop(); // removes 30 from the last 
+myarr.unshift(20) // adds 20 before 1
+myarr.shift() // removes 20 before 1
+const join1 = myarr.join()
+console.log(join1) // type of array is changed to string
+
+//slice, spice
+
